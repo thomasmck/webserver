83b4425eda0ee9263124b194601a80a3ec03758d
Thomas Mckelvey <tenacioust4@msn.com>
2017-03-10 21:10:32 +0000

    Initial commit
    Signed-off-by: Thomas Mckelvey <tenacioust4@msn.com>

---
+++ slack_handler.py
@@ -0,0 +1,65 @@
+import cgi
+
+"""imple HTTP server in python.
+Usage::
+    ./dummy-web-server.py [<port>]
+Send a GET request::
+    curl http://localhost
+Send a HEAD request::
+    curl -I http://localhost
+Send a POST request::
+    curl -d "foo=bar&bin=baz" http://localhost
+"""
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+
+class S(BaseHTTPRequestHandler):
+
+    def handle_POST_data(self, data):
+        print data 
+        ctype, pdict = data
+        if ctype == 'multipart/form-data':
+            postvars = cgi.parse_multipart(self.rfile, pdict)
+        elif ctype == 'application/x-www-form-urlencoded':
+            length = int(self.headers.getheader('content-length'))
+            postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+        else:
+            postvars = {}
+        return self.format_data(postvars)
+
+    def format_data(self, postvars):
+        var_list = []
+        for key, value in postvars.iteritems():
+            var_list.append(value[0])
+        return var_list
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
+    def do_HEAD(self):
+        self._set_headers()
+        
+    def do_POST(self):
+        self._set_headers()
+        postvars = self.handle_POST_data(cgi.parse_header(self.headers.getheader('content-type')))
+        self.wfile.write("<html><body><h1>%s</h1></body></html>" %postvars)
+
+def run(server_class=HTTPServer, handler_class=S, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print 'Starting httpd...'
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
